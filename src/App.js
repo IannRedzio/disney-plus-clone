f76c740ef7673bfce6d09a7cd5729ede99c13173
IannRedzio <ianredzio13@gmail.com>
2022-06-23 09:44:13 -0300

    add detail page, add react-router-dom, add movi-dom, add movies cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Detail from "./pages/Detail/Detail";
 import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
